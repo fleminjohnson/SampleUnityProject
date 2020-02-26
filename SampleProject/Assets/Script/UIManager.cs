a34329edf84827b1be4d6268d30300ee3a39ba94
fjeminjohnson <fjfleminjohnson@gmail.com>
2020-02-26 21:17:08 +0530

    Edit in Branch

---
+++ SampleProject/Assets/Script/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void SceneChanger(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+    }
+}
+
